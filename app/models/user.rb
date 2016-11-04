f31e4f506227bb2cd86746cf4cdf265664df3461
faraquet <yaidzo@yandex.ru>
2016-11-04 05:01:21 +0500

    Change User model for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :omniauthable, omniauth_providers: [:vkontakte]
+  
+  def remember_me
+    true
+  end
+  
+  def self.from_vkontakte(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+    end
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
 end
